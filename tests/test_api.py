34daec4073d0e231d1abfd3358d6d1671d1b5417
Duarte OC <me@duarteocarmo.com>
2023-08-23 14:15:07 +0200

    api and test workign

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,8 +8,3 @@ class TestAPI:
     def test_read_root(self):
         response = client.get("/")
         assert response.status_code == 200
-
-    def test_read_hello(self):
-        response = client.post("/hello")
-        assert response.status_code == 200
-        assert "We are here" in response.text
