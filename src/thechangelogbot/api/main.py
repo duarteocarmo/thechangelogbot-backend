c41784f457480ac99b36487a0e816efa5c5185d3
Duarte OC <me@duarteocarmo.com>
2023-09-12 13:05:37 +0200

    rate limituing

--- src/thechangelogbot/api/main.py
+++ src/thechangelogbot/api/main.py
@@ -1,11 +1,13 @@
+import time
 from typing import Optional
 
 import pkg_resources
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse, StreamingResponse
 from loguru import logger
 from pydantic import BaseModel
+from ratelimiter import RateLimiter
 from thechangelogbot.conf.load_config import config
 from thechangelogbot.index.chat import respond_to_query
 from thechangelogbot.index.database import (
@@ -15,19 +17,35 @@ from thechangelogbot.index.database import (
 )
 
 DATABASE, COLLECTION = get_superduperdb_components(config)
-
+RATE_LIMIT_CALLS, RATE_LIMIT_SECONDS = 10, 60
 
 app = FastAPI(
     title="Changelogbot API",
     version=pkg_resources.get_distribution("thechangelogbot").version,
 )
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=config["api"]["origins"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+)
+
+
+def limited(until):
+    duration = int(round(until - time.time()))
+    logger.info(f"Sleeping for {duration:.2f} seconds")
+    raise HTTPException(
+        status_code=429,
+        detail=f"Rate limited, sleeping for {duration:.2f} seconds",
+    )
+
+
+rate_limiter_search = RateLimiter(
+    max_calls=RATE_LIMIT_CALLS, period=RATE_LIMIT_SECONDS, callback=limited
+)
+rate_limiter_chat = RateLimiter(
+    max_calls=RATE_LIMIT_CALLS, period=RATE_LIMIT_SECONDS, callback=limited
 )
 
 
@@ -55,15 +73,16 @@ class SearchRequest(BaseModel):
 
 @app.post("/search")
 def search_endpoint(request: SearchRequest):
-    return search_database(
-        config=config,
-        query=request.query,
-        list_of_filters=request.filters,
-        limit=request.limit,
-        db=DATABASE,
-        collection=COLLECTION,
-        initialize=False,
-    )
+    with rate_limiter_search:
+        return search_database(
+            config=config,
+            query=request.query,
+            list_of_filters=request.filters,
+            limit=request.limit,
+            db=DATABASE,
+            collection=COLLECTION,
+            initialize=False,
+        )
 
 
 @app.get("/podcasts")
@@ -96,14 +115,15 @@ def speakers():
 
 @app.post("/chat")
 def chat(request: ChatRequest):
-    logger.info(f"Query: {request}")
-    return StreamingResponse(
-        respond_to_query(
-            query=request.query,
-            speaker=request.speaker,
-            db=DATABASE,
-            config=config,
-            collection=COLLECTION,
-        ),
-        media_type="text/plain",
-    )
+    with rate_limiter_chat:
+        logger.info(f"Query: {request}")
+        return StreamingResponse(
+            respond_to_query(
+                query=request.query,
+                speaker=request.speaker,
+                db=DATABASE,
+                config=config,
+                collection=COLLECTION,
+            ),
+            media_type="text/plain",
+        )
